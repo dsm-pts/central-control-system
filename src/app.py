70a44a2e2c00832409235d0bce8638aebebffaab
Yunseo Hwang <yunseo.dev@gmail.com>
2021-06-20 16:32:45 +0900

    Add constants and add decoding function
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/app.py
+++ src/app.py
@@ -2,24 +2,35 @@ from serial_watcher import SerialWatcher
 from get_ser import serial_ports
 import constants
 
-def watch(data, serial, serials, serial_objs):
-    print(serial, ': rx', data)
-
-def make_msg_with_protocol(type, command, data):
+def message_encoding(sender, command, data):
     # Protocol
-    # Start(0x02) Type(1 byte) Command(1 byte) Data_H8(1 byte) Data_L8(1 byte) End(0x03)
+    # Start(0x02) Sender(1 byte) Command(1 byte) Data_H8(1 byte) Data_L8(1 byte) End(0x03)
 
     data_h8 = (data & 0xFF00) >> 8
     data_l8 = data & 0x00FF
 
-    type_byte = type.to_bytes(1, 'big')
+    sender_byte = sender.to_bytes(1, 'big')
     command_byte = command.to_bytes(1, 'big')
     data_h8_byte = data_h8.to_bytes(1, 'big')
     data_l8_byte = data_l8.to_bytes(1, 'big')
 
-    return b'\x02' + type_byte + command_byte + data_h8_byte + data_l8_byte + b'\x03'
+    return b'\x02' + sender_byte + command_byte + data_h8_byte + data_l8_byte + b'\x03'
 
+def message_decoding(msg):
+    if len(msg) < 5:
+        print('invalid protocol')
+    sender = msg[1]
+    command = msg[2]
 
+    data_h8 = msg[3]
+    data_l8 = msg[4]
+
+    data = (data_h8 << 8) | data_l8
+
+    return (sender, command, data)
+
+def watch(data, serial, serials, serial_objs):
+    print(serial, ': rx', data)
 
 def main():
     watcher = SerialWatcher(watch)
@@ -31,7 +42,7 @@ def main():
     watcher.watch_start()
     running = True
 
-    watcher.broadcast(make_msg_with_protocol(constants.TYPE_RPI, constants.CMD_SET_SECONDS, 31))
+    watcher.broadcast(message_encoding(constants.TYPE_RPI, constants.CMD_SET_SECONDS, 31))
 
     while running:
         try:
